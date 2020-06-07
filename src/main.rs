cc8b8e1cd722e120d0aab947b8c9a11146acd14a
Pierre Roudier <pierre@roudier.io>
2020-06-06 22:02:57 -0400

    Added vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,74 @@
 extern crate glfw;
 
 use glfw::{Action, Context, Key, WindowHint};
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Failed to initialize GLFW library.");
 
+    let app_infos = vulkano::app_info_from_cargo_toml!();
+    let instance = Instance::new(Some(&app_infos), &InstanceExtensions::none(), None)
+        .expect("Failed to create Vulkan instance");
+
+    println!("Dumping Vulkan compatible devices:");
+    for phy_dev in PhysicalDevice::enumerate(&instance) {
+        println!(
+            "  - index: {}, name: {}, api version: {}, driver version: {}",
+            phy_dev.index(),
+            phy_dev.name(),
+            phy_dev.api_version(),
+            phy_dev.driver_version()
+        );
+    }
+    let physical = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("No device compatible with Vulkan are available");
+
+    println!("Dumping memory heaps:");
+    for memory_heap in physical.memory_heaps() {
+        println!(
+            "  - id: {}, size (Mb): {}, device local: {}",
+            memory_heap.id(),
+            memory_heap.size() / (1024 * 1024),
+            memory_heap.is_device_local()
+        );
+    }
+
+    println!("Dumping queue families:");
+    for que_fam in physical.queue_families() {
+        println!(
+            "  - id: {}, queue count: {}, graphical: {}",
+            que_fam.id(),
+            que_fam.queues_count(),
+            que_fam.supports_graphics()
+        );
+    }
+    let queue_family = physical
+        .queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+
+    let (device, mut queues) = {
+        Device::new(
+            physical,
+            &Features::none(),
+            &DeviceExtensions::none(),
+            [(queue_family, 0.5)].iter().cloned(),
+        )
+        .expect("failed to create device")
+    };
+    let queue = queues.next().unwrap();
+
+
     glfw.window_hint(WindowHint::Resizable(false));
     glfw.window_hint(WindowHint::DoubleBuffer(true));
-    let (mut window, events) = glfw.create_window(1920, 1080, "Vulkust", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw
+        .create_window(1920, 1080, "Vulkust", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.set_key_polling(true);
@@ -23,9 +84,7 @@ fn main() {
 
 fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
     match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
         _ => {}
     }
 }
